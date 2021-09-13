1dff5feb53c92c53d0a0a6a6557b67af01527d46
Werick Nalyson <wericknalyson@gmail.com>
2021-09-12 21:46:01 -0300

    feat: add validation inputs

--- src/modules/Product/routes/product.routes.ts
+++ src/modules/Product/routes/product.routes.ts
@@ -1,13 +1,57 @@
 import { Router } from 'express';
 import ProductsController from '../controller/ProductsController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productsRouter = Router();
 const productsController = new ProductsController();
 
-productsRouter.post('/', productsController.create);
 productsRouter.get('/', productsController.index);
-productsRouter.get('/:id', productsController.show);
-productsRouter.patch('/:id', productsController.update);
-productsRouter.delete('/:id', productsController.delete);
+
+productsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().required(),
+    },
+  }),
+  productsController.show,
+);
+
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productsController.create,
+);
+
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.number().required(),
+    },
+  }),
+  productsController.update,
+);
+
+productsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().required(),
+    },
+  }),
+  productsController.delete,
+);
 
 export default productsRouter;
